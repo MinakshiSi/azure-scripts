9744f3c7e87e18ca32cd05a6fb45518c703b7394
Minakshi Singh <minakshisingh9320@gmail.com>
2025-10-28 17:05:40 +0530

    Update install-agent.ps1

--- install-agent.ps1
+++ install-agent.ps1
@@ -4,11 +4,18 @@ $orgUrl = "https://dev.azure.com/MissonDevopsCertification"
 $pat = "DC8AAChdx2DWLsSxi22LBZXIBNretHD0ahroMtPAwgk3qFeruN53JQQJ99BJACAAAAAAAAAAAAASAZDO27AX"
 $pool = "MoodPulsePool"
 
+# Create agent directory
 New-Item -ItemType Directory -Force -Path $agentDir
+
+# Download and extract agent
 Invoke-WebRequest -Uri $agentUrl -OutFile "$agentDir\agent.zip"
 Expand-Archive "$agentDir\agent.zip" -DestinationPath $agentDir
 
+# Configure agent
 cd $agentDir
 .\config.cmd --unattended --url $orgUrl --auth pat --token $pat --pool $pool --acceptTeeEula
-.\svc.sh install
-.\svc.sh start
+
+# Install and start agent as Windows service
+.\svc install
+.\svc start
+
