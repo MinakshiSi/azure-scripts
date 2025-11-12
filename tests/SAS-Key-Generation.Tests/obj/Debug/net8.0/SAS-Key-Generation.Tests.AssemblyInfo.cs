131b8bc5636df92310311fcdfdf5f859321cbc4e
Minakshi Singh <u464605@wns.com>
2025-11-12 12:30:06 +0530

    MSBuild import dependency removed

--- tests/SAS-Key-Generation.Tests/obj/Debug/net8.0/SAS-Key-Generation.Tests.AssemblyInfo.cs
+++ tests/SAS-Key-Generation.Tests/obj/Debug/net8.0/SAS-Key-Generation.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SAS-Key-Generation.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c927c71df965ba26562a32ad141405635bfbf4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ccac61d58e334b156e30bfd1c3281418d6ca661d")]
 [assembly: System.Reflection.AssemblyProductAttribute("SAS-Key-Generation.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SAS-Key-Generation.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
