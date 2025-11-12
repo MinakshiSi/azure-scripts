b1455809d9b85fd2bad94ad87d39fa9c5748dc42
Minakshi Singh <u464605@wns.com>
2025-11-12 14:56:38 +0530

    IStorageHelper reference added in Program

--- src/sas-key-generation-service/Program.cs
+++ src/sas-key-generation-service/Program.cs
@@ -1,7 +1,9 @@
+using SAS_Key_Generation.Helpers;
 using sas_key_generation_service;
 
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddHostedService<Worker>();
+builder.Services.AddSingleton<IStorageHelper, StorageHelper>();
 
 var host = builder.Build();
 host.Run();
